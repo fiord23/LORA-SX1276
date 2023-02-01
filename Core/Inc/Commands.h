90e857689956a037b49e613f94a892870f077da2
Dzmitry <fiord231@gmail.com>
2023-02-01 12:55:47 +0300

    /FW works

--- Core/Inc/Commands.h
+++ Core/Inc/Commands.h
@@ -2,4 +2,6 @@
 #define __COMMANDS_H
 #include <stdbool.h>
 bool Lora_Show_List_of_Commands (void);
+bool Parser_Commands (void);
+void Uart_Data_Clear (void);
 #endif
--- Core/Src/Commands.c
+++ Core/Src/Commands.c
@@ -10,7 +10,7 @@ extern uint8_t str_uart[RX_BUFFER_SIZE];
 bool Lora_Show_List_of_Commands (void)
 {  
   uint8_t help_compare[] = "/help\n"; 
-  uint8_t list_of_commands[] = "LIST OF COMMANDS\r\n";
+  uint8_t list_of_commands[] = "\r\n--------------------\r\nLIST OF COMMANDS\r\n";
   uint8_t set_data_to_reg[] = "/Set - Set in reg(0xAB) data (0x55). Example:/Set 0xAB 0x55\r\n";
   uint8_t read_data_from_reg[] = "/Read - Read from reg(0xAB) data (0x55). Example:/Read 0xAB 0x55\r\n";
   uint8_t lora_config_data[] = "/Config - show current lora config\r\n";
@@ -18,7 +18,7 @@ bool Lora_Show_List_of_Commands (void)
   uint8_t who[] = "/Who - to get list  ID available lora online \r\n";
   uint8_t send_target_message[] = "Msg ID text - send message target ID lora. Example: msg 0x18 Hello!\r\n";
   uint8_t send_all_lora_message[] = "Msg 0 text - send message all available lora. Example: msg 0x00 Hello!\r\n";
-  if (!strncmp(str_uart, help_compare, sizeof(help_compare)))
+  if (!strcmp(str_uart, help_compare))
     {
       HAL_UART_Transmit(&huart2, list_of_commands, sizeof(list_of_commands), 100);
       HAL_UART_Transmit(&huart2, set_data_to_reg, sizeof(set_data_to_reg), 100);
@@ -28,9 +28,28 @@ bool Lora_Show_List_of_Commands (void)
       HAL_UART_Transmit(&huart2, who, sizeof(who), 100);
       HAL_UART_Transmit(&huart2, send_target_message, sizeof(send_target_message), 100);
       HAL_UART_Transmit(&huart2, send_all_lora_message, sizeof(send_all_lora_message), 100);
+      Uart_Data_Clear();
       return true;
     }
     else 
-      return false;
+      return false; 
+}
+
+bool Parser_Commands (void)
+{
+  uint8_t firmware_version[] = "/FW\n";
+  if (!strcmp(str_uart, firmware_version))
+  {
+    Lora_Show_Firmware_Version();  
+    Uart_Data_Clear();
+    return true;
+  }
   
+  else 
+    return false;
+}
+void Uart_Data_Clear (void)
+{
+      for (volatile uint8_t a = 0; a< RX_BUFFER_SIZE; a ++)
+        *(str_uart + a) = 0;
 }

4780712647fb2a24bb42fd767f4d1ed75956282e
Alexey <deadline@mail.ru>
2023-02-03 18:03:32 +0300

    Change help list 2

--- Core/Src/Commands.c
+++ Core/Src/Commands.c
@@ -45,24 +45,18 @@ bool Parser_Commands (void)
 
 void Lora_Show_List_of_Commands (void)
 {  
-  uint8_t list_of_commands[] = "\r\n--------------------\r\nLIST OF COMMANDS\r\n";
-  uint8_t set_data_to_reg[] = "/Set - Set in reg(0xAB) data (0x55). Example:/Set 0xAB 0x55\r\n";
-  uint8_t read_data_from_reg[] = "/Read - Read from reg(0xAB) data (0x55). Example:/Read 0xAB 0x55\r\n";
-  uint8_t lora_config_data[] = "/Config - show current lora config\r\n";
-  uint8_t get_FW[] = "/FW - Show firmware version\r\n";
-  uint8_t who[] = "/Who - to get list  ID available lora online \r\n";
-  uint8_t send_target_message[] = "Msg ID text - send message target ID lora. Example: msg 0x18 Hello!\r\n";
-  uint8_t send_all_lora_message[] = "Msg 0 text - send message all available lora. Example: msg 0x00 Hello!\r\n";
+  uint8_t list_of_commands[] = "\r\n--------------------\r\nLIST OF COMMANDS\r\n \
+/Set - Set in reg(0xAB) data (0x55). Example:/Set 0xAB 0x55\r\n \
+/Read - Read from reg(0xAB) data (0x55). Example:/Read 0xAB 0x55\r\n \
+/Config - show current lora config\r\n \
+/FW - Show firmware version\r\n \
+/Who - to get list  ID available lora online \r\n \
+Msg ID text - send message target ID lora. Example: msg 0x18 Hello!\r\n \
+Msg 0 text - send message all available lora. Example: msg 0x00 Hello!\r\n";
   HAL_UART_Transmit(&huart2, list_of_commands, sizeof(list_of_commands), 100);
-  HAL_UART_Transmit(&huart2, set_data_to_reg, sizeof(set_data_to_reg), 100);
-  HAL_UART_Transmit(&huart2, read_data_from_reg, sizeof(read_data_from_reg), 100);
-  HAL_UART_Transmit(&huart2, lora_config_data, sizeof(lora_config_data), 100);
-  HAL_UART_Transmit(&huart2, get_FW, sizeof(get_FW), 100);
-  HAL_UART_Transmit(&huart2, who, sizeof(who), 100);
-  HAL_UART_Transmit(&huart2, send_target_message, sizeof(send_target_message), 100);
-  HAL_UART_Transmit(&huart2, send_all_lora_message, sizeof(send_all_lora_message), 100);
   Uart_Data_Clear();
 }
+
 void Command_FW(void)
 {
     Lora_Show_Firmware_Version();  
